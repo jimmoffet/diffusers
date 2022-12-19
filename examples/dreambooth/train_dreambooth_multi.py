77cebc68536d33815f075bf610a4b77852dcdc08
jimmoffet <jim.moffet@gsa.gov>
2022-12-19 01:28:49 -0800

    run training from main

--- examples/dreambooth/train_dreambooth_multi.py
+++ examples/dreambooth/train_dreambooth_multi.py
@@ -483,8 +483,7 @@ def run_training(args_imported):
             for example in tqdm(
                 sample_dataloader, desc="Generating class images", disable=not accelerator.is_local_main_process
             ):
-                with torch.autocast("cuda"):
-                    images = pipeline(example["prompt"]).images
+                images = pipeline(example["prompt"]).images
 
                 for i, image in enumerate(images):
                     image.save(class_images_dir / f"{example['index'][i] + cur_class_images}.jpg")
